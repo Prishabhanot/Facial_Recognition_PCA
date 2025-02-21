5f71b9ae4c16537f6dc8c8b0d49249ec9f8c3e60
Prisha Bhanot <bhanop1@mcmaster.ca>
2025-02-21 03:22:57 -0500

    Importing libararies

--- Code.py
+++ Code.py
@@ -1,2 +1,10 @@
 #Data set includes 40 people, and 10 images of them with different expressions (wearing glasses), to increase variation
 
+import numpy as np
+import pandas as pd 
+import matplotlib.pyplot as plt 
+from time import time 
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from sklearn.decomposition import PCA 
+from sklearn.svm import SVC 
